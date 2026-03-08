8abfcb42083a3231cfda1997d9531c6cbd2ed594
OrNatan <ornatan2222@gmail.com>
2026-03-09 01:58:01 +0200

    describe the change

--- app/server.js
+++ app/server.js
@@ -3,11 +3,26 @@ const http = require("http");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Home page\n");
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Esti Ella Natan I love You\n");
+  } else if (req.url === "/test") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Test page\n");
+
+  } else if (req.url === "/health") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ status: "ok" }));
+
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Page not found\n");
+  }
 });
 
 server.listen(port, () => {
